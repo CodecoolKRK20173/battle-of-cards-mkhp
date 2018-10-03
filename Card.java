9330a262cb909c985d0b19455cc9138881855f7d
HUbert Strumiński <hubert.struminski@gmail.com>
2018-10-03 11:47:36 +0200

    add classes Card, CardController with their functions

---
+++ Card.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Card{
+
+    private String type;
+    private String procent;
+    private String wrapping;
+    private String hangover;
+
+    private String name;
+    private Map<String, Integer> stats = new HashMap<>();
+
+    public Card(){
+
+    }
+    
+    public Card(String name, String type, String procent, String wrapping, String hangover){
+        this.type = type;
+        this.procent = procent;
+        this.wrapping = wrapping;
+        this.hangover = hangover;
+        this.name=name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Map<String, Integer> getStats(){
+        return this.stats;
+    }
+
+    public Card getCard(Card card){
+        return card;
+    }
+}
